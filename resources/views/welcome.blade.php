21d2d28be0188341b6d924e82f1a1a3e6f94e9fc
elahe <elahe.khabaz.bavani@gmail.com>
2023-03-26 06:33:14 +0430

    f

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <!-- <script src="/app.js"></script> -->
+    <title>my blog</title>
+
+</head>
+<body>
+<article>
+    <h1><a href="/post"> my first post</a> </h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis recusandae, praesentium cum nostrum illum quasi, id cupiditate voluptatem odio aliquam impedit molestiae temporibus sit modi suscipit sint atque expedita explicabo.
+    </p>
+</article>
+
+<article>
+    <h1><a href="/post"> my second post </a></h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis recusandae, praesentium cum nostrum illum quasi, id cupiditate voluptatem odio aliquam impedit molestiae temporibus sit modi suscipit sint atque expedita explicabo.
+    </p>
+</article>
+
+
+<article>
+    <h1><a href="/post"> my third post </a></h1>
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis recusandae, praesentium cum nostrum illum quasi, id cupiditate voluptatem odio aliquam impedit molestiae temporibus sit modi suscipit sint atque expedita explicabo.
+    </p>
+</article>
+
+
+</body>
+</html>
