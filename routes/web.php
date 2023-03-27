681cd1b7c5a232260e894643e42bb3a238cf813f
amani <elahe.khabaz.bavani@gmail.com>
2023-03-27 11:15:28 +0330

    change1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use PhpParser\Node\Stmt\Catch_;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,20 +19,25 @@ Route::get('/posts', function () {
 });
 
 
-Route::get('/posts/{post}', function ($slug) {
-
+Route::get('/posts/{post}', function ($slug) { /////در این بخش از کد ما {post} راداریم که چون داخل کرلی براکت هست هربار یک فایل را میگیرد
+///////اسلاگ همون بخش بعد از دامنه است
     $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
 if(! file_exists($path)){
-    return redirect('/posts');
-    // abort(404);
+    return redirect('/posts');///////دستور ریدایرکت
+    // abort(404);    /////اگر خواستم صفحه ۴۰۴ یا ۵۰۰ یا ۴۰۳ و یا هر چیز دیگه از این دستورز استفاده کن
 }
+$post= cache()->remember("posts.{$slug}",5,function() use ($path){
+ 
+    var_dump('file_get_contents');
+    return file_get_contents($path);
 
-$post=file_get_contents($path);
+});
+
+// $post=file_get_contents($path);
     return view('post', [
         'post' => $post
-    
     ]);
-});
+})->where('post', '[A-z_/-]+'); ////در این بخش قراردادی تنظیم شده برای بخش اسلاگ که توی اسلاگ میتونی حروف بزرگ و کوچک و ـ و- قرار بدهی یکسری تابع عم هست whereAlpha() , whereNumber()
 
 
