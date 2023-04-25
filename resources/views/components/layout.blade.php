89da4b676f4518a0405afa0fc1a73076456ef49b
Elahe <elahe.khabaz.bavani@gmail.com>
2023-04-25 12:08:44 +0330

    register button

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,6 +22,11 @@
                 <a href="#" class="bg-blue-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
                     Subscribe for Updates
                 </a>
+
+                <a href="/register" class="bg-green-500 ml-3 rounded-full text-xs font-semibold text-white uppercase py-3 px-5">
+                    Register
+                </a>
+
             </div>
         </nav>    
 		   
