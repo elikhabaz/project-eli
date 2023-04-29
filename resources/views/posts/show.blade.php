c1154d11efb8412424ab2516b2481baa920e8bcf
Elahe <elahe.khabaz.bavani@gmail.com>
2023-04-29 12:11:08 +0330

    when user doesn't login into the page we have two link for sign in and sign up

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,9 +50,11 @@
                     </div>
 
                 </div>
-                    <section class=" col-span-8 col-start-5  mt-10 space-y-10">
-                <x-penal >
-                    <form method="POST" action="/posts/{{$post->slug}}/comment" >
+                <section class=" col-span-8 col-start-5  mt-10 space-y-10">
+                    @auth 
+                    <!-- یوزر تا زمانی که لاگین نباشه نمیتونه نظری رو درج کنه از auth استفاده کردم و در غیر این صورت براش لینک لاگین گذاشتم -->
+                    <x-penal >
+                     <form method="POST" action="/posts/{{$post->slug}}/comment" >
                         @csrf
                         <header calss="flex item-center">
                         <img src="https://i.pravatar.cc/40?u{{auth()->id()}}" width="40px" hight="40px" class="rounded-full">
@@ -71,8 +73,20 @@
                             </div>
                         </form>
                         </x-penal>
+                        @else
+                        <div class="flex">
+                        <p>
+                            <a href="/login" class="px-3 py-1 border bg-purple-600 border-purple-300 rounded-full text-white text-xs uppercase font-semibold">please sign in to laracast</a>
+                        </p>
 
-                       
+                        <p class="ml-6">
+                            <a href="/register" class="px-3 py-1 border bg-green-600 border-green-300 rounded-full text-white text-xs uppercase font-semibold">please sign up to laracast</a>
+                        </p>
+                        </div>
+                        
+                        @endauth
+
+                       <!-- این پسی که باید بدی از سمت کامپوننت خیلی مهمه و همیشه باید مراقب باشی هست یا نه  -->
                            @foreach($post->comments as $comment)
                               <x-comment :comment="$comment"/>
                            @endforeach
